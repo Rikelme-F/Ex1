d38958bdca283e579d90d166f5c6e714ee924d82
Rikelme-F <Rikelme.fsantos@senacsp.edu.br>
2025-03-20 20:13:58 -0300

    Adicionar arquivos de projeto.

---
+++ Exercício1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercício1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva um número");
+            string numero1 = Console.ReadLine();
+
+            Console.WriteLine("Escreva mais um número");
+            string numero2 = Console.ReadLine();
+
+            int num1 = int.Parse(numero1);
+            int num2 = int.Parse(numero2);
+
+
+            int soma = num1 + num2;
+            Console.WriteLine("A soma dos números é: " + soma);
+        }
+    }
+}
+
